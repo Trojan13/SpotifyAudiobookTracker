76694515cdde84c1439135abab7c3174461e7d63
TPack <trojan9418@gmail.com>
2025-12-19 18:02:26 +0100

    Disable app manifest to fix build errors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   
+  app: {
+    buildAssetsDir: '/_nuxt/',
+  },
+
+  experimental: {
+    appManifest: false
+  },
+  
   devServer: {
     host: '127.0.0.1',
     port: 3000
